7216c69e8e83dff2e731eccde9596027e2737456
allen <eepson123@gmail.com>
2024-10-26 16:19:26 +0800

    feat: add python env test book

---
+++ python/main-p1-env.py
@@ -0,0 +1,24 @@
+print("hello Python")
+
+
+def hello(v0,t):
+	return v0*t+0.5*9.81*t**2
+	
+print(hello(5,2))
+
+def buy_book(title):
+    print(f"買了一本書：{title}")
+
+## type python in terminal and it will print out Read-Eval-Print Loop
+
+## source code >> source dballz/bin/activate
+
+## deactivate >> deactivate
+
+## pip will be a install tools for python
+
+## to make env out put to a file >> pip freeze > requirements.txt
+
+## pip install -r requirements.txt
+
+## poetry env info https://python-poetry.org/docs/dependency-specification/#version-constraints
